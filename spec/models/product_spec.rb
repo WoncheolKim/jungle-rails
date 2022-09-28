dd307f8d5c6f9b5deccc63572457b89142d7aeae
Woncheol Kim <icijiy@gmail.com>
2022-09-27 21:55:48 -0700

    Create Validation specs

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,102 @@
+require 'rails_helper'
+
+# validates :name, presence: true
+# validates :price, presence: true
+# validates :quantity, presence: true
+# validates :category, presence: true
+
+RSpec.describe Product, type: :model do
+
+  describe 'Validations' do
+    
+    it "should have valid attribute: name, price, quantity and category" do
+      @category = Category.new
+      @category.id = 2
+      @category.name = "test"
+
+      @product = Product.new
+      @product.name = "test"
+      @product.price = 100
+      @product.quantity = 100
+      @product.category_id = @category
+
+      @category.products = [@product]
+
+      @product.save
+
+      expect(@product).to be_valid
+    end
+
+    it "should have the product name" do
+      @category = Category.new
+      @category.id = 2
+      @category.name = "test"
+
+      @product = Product.new
+      @product.name = nil
+      @product.price = 100
+      @product.quantity = 100
+      @product.category_id = @category
+
+      @category.products = [@product]
+
+      @product.save
+
+      expect(@product).to_not be_valid
+    end
+
+    it "should have the product price" do
+      @category = Category.new
+      @category.id = 2
+      @category.name = "test"
+
+      @product = Product.new
+      @product.name = "test"
+      @product.price_cents = nil
+      @product.quantity = 100
+      @product.category_id = @category
+
+      @category.products = [@product]
+
+      @product.save
+
+      expect(@product).to_not be_valid
+    end
+
+    it "should have the product quantity" do
+      @category = Category.new
+      @category.id = 2
+      @category.name = "test"
+
+      @product = Product.new
+      @product.name = "test"
+      @product.price = 100
+      @product.quantity = nil
+      @product.category_id = @category
+
+      @category.products = [@product]
+
+      @product.save
+
+      expect(@product).to_not be_valid
+    end
+
+    it "should have the product in correct category" do
+       @category = Category.new
+      @category.id = 2
+      @category.name = "test"
+
+      @product = Product.new
+      @product.name = nil
+      @product.price = 100
+      @product.quantity = 100
+      @product.category_id = nil
+
+      @category = [@product]
+
+      @product.save
+
+      expect(@product).to_not be_valid
+    end
+  end
+end

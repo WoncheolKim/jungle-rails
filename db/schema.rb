784ba5e280cb2c6a64eafb22f969c02532b5fa77
Woncheol Kim <icijiy@gmail.com>
2022-09-27 23:04:01 -0700

    Add tests for User authentication

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,8 @@ ActiveRecord::Schema.define(version: 2022_09_26_024246) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
